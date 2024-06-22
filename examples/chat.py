4ef636e7442e0dfbadc0011b210cd9e1b9f2eee4
Satpal Singh Rathore <satpal.code@gmail.com>
2024-06-22 12:01:01 +0530

    add llm

---
+++ examples/chat.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(current_dir, "../src"))
+
+from dataformer.llms.openllm import OpenLLM
+from datasets import load_dataset
+from dotenv import load_dotenv
+load_dotenv()
+
+dataset = load_dataset("dataformer/self-knowledge")
+datasetsub = dataset['train'].select(range(5))
+instructions = [example['question'] for example in datasetsub]
+
+model = "gpt-3.5-turbo"
+request_list = [{"model": model, "messages": [{"role": "user", "content": prompt}]} for prompt in instructions]
+
+llm = OpenLLM()
+response_list = llm.generate(request_list, save_filepath = 'chat.jsonl')
+
+for request, response in zip(request_list, response_list):
+    prompt = request['messages'][0]['content']
+    answer = response[-1]['choices'][0]['message']['content']
+    print(f"Prompt: {prompt}\nAnswer: {answer}\n")
