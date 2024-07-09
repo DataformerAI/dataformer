fc2fe7b2dd2dc413afa59012904135fa9661af40
Manav <manavmanoj99@gmail.com>
2024-07-09 17:56:58 +0530

    add deita filtering

--- src/dataformer/components/__init__.py
+++ src/dataformer/components/__init__.py
@@ -2,5 +2,6 @@ from dataformer.components.evol_instruct.base import EvolInstruct
 from dataformer.components.evol_quality.base import EvolQuality
 from dataformer.components.complexity_scorer import ComplexityScorer
 from dataformer.components.quality_scorer import QualityScorer
+from dataformer.components.deita import Deita
 
-__all__ = [EvolInstruct, EvolQuality, ComplexityScorer, QualityScorer]
+__all__ = [EvolInstruct, EvolQuality, ComplexityScorer, QualityScorer, Deita]
