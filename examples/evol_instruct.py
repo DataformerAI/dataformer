d61cd032a5356a660349a70a802edf0377b541f9
Sourabh singh <96938361+SouSingh@users.noreply.github.com>
2024-07-16 12:47:52 +0530

    Update evol_instruct.py

--- examples/evol_instruct.py
+++ examples/evol_instruct.py
@@ -5,6 +5,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+COLOR = {
+    "RED": "\033[91m",
+    "GREEN": "\033[92m",
+    "YELLOW": "\033[93m",
+    "BLUE": "\033[94m",
+    "PURPLE": "\033[95m",
+    "CYAN": "\033[96m",
+    "WHITE": "\033[97m",
+    "ENDC": "\033[0m",
+}
+
 dataset = load_dataset("dataformer/self-knowledge")
 datasetsub = dataset["train"].select(range(2))
 instructions = [example["question"] for example in datasetsub]
@@ -24,4 +35,4 @@ results = evol_instruct.generate(
     instructions, use_cache=False
 )  # By default cache is True.
 
-print(results)
+print(f"{COLOR['GREEN']}{results}{COLOR['ENDC']}")
