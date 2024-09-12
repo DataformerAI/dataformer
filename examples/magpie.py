fa148044ad48eb7bb1c650a5ff71164378385130
Satpal Singh Rathore <satpal.code@gmail.com>
2024-09-12 18:22:50 +0530

    magpie QA

--- examples/magpie.py
+++ examples/magpie.py
@@ -1,23 +1,62 @@
+import os
 import json
-
 from dataformer.components import MAGPIE
 from dataformer.llms import AsyncLLM
 from dotenv import load_dotenv
-# Load environment variables from .env file
 load_dotenv()
+
+api_key = os.getenv("JARVIS_API_KEY")
+
 # Ollama - openai compatible endpoint
-# Template - https://jarvislabs.ai/templates/ollama
-# Once you create an instance, click on API - to get Endpoint URL & Deploy as such: https://jarvislabs.ai/blogs/ollama_deploy (ollama pull llama3)
-URL = "https://a8da29c1850e1.notebooksa.jarvislabs.net/v1/chat/completions"
+# Deploy via https://jarvislabs.ai/serverless/frameworks/ollama_serverless
+URL = "https://api.jarvislabs.net/openai/900f66f0cd836278/v1/chat/completions"
+
+question_prompt = """
+"You are an AI assistant specialized in logical thinking and problem-solving.
+Your purpose is to help users work through complex ideas, analyze situations, and draw conclusions based on given information.
+Approach each query with structured thinking, break down problems into manageable parts, and guide users through the reasoning process step-by-step."
+"""
+
+answer_prompt = """
+You're an AI assistant that responds to the user with maximum accuracy.
+To do so, your first task is to think what the user is asking for, thinking step by step.
+During this thinking phase, you will have reflections that will help you clarifying ambiguities.
+In each reflection you will list the possibilities and finally choose one. Between reflections, you can think again.
+At the end of the thinking, you must draw a conclusion.
+You only need to generate the minimum text that will help you generating a better output, don't be verbose while thinking.
+Finally, you will generate an output based on the previous thinking.
+
+This is the output format you have to follow:
+
+```
+<thinking>
+Here you will think about what the user asked for.
+</thinking>
+
+<reflection>
+This is a reflection.
+</reflection>
+
+<reflection>
+This is another reflection.
+</reflection>
+
+</thinking>
+
+<output>
+Here you will include the output.
+</output>
+```
+""".lstrip()
 
 sampling_params = {"temperature": 0.6, "top_p": 1}
-llm = AsyncLLM(model="llama3", url=URL, sampling_params=sampling_params, api_provider="ollama", max_requests_per_minute=5)
+llm = AsyncLLM(model="llama3.1", url=URL, sampling_params=sampling_params, max_requests_per_minute=5, api_key=api_key)
 
 # Only user template, NOT the entire chat template.
 template = "<|begin_of_text|><|start_header_id|>user<|end_header_id|>"
-magpie = MAGPIE(llm, template=template)
+magpie = MAGPIE(llm, template=template, question_prompt=None, answer_prompt=None)
 num_samples=5 # Actual samples can be bit less since we filter out any empty responses.
 dataset = magpie.generate(num_samples)
 
 with open("dataset.json", "w") as json_file:
-    json.dump(dataset, json_file, indent=4)
+    json.dump(dataset, json_file, indent=4, ensure_ascii=False)
--- src/dataformer/components/magpie/base.py
+++ src/dataformer/components/magpie/base.py
@@ -1,25 +1,30 @@
 from dataformer.components.magpie.prompts import languages, templates
 
-
 class MAGPIE:
-    def __init__(self, llm, template=None, lang="en"):
+    def __init__(self, llm, template=None, lang="en", question_prompt=None, answer_prompt=None):
         self.llm = llm
         self.lang = lang
+        self.question_prompt = question_prompt
+        self.answer_prompt = answer_prompt
 
         if template:
             self.template = template
-
         else:
             if "llama3" or "llama-3" in self.llm.model:
                 self.template = templates["llama3"]
 
         self.template = f"{self.template}{languages[self.lang]}:"
 
-    def create_requests(self, prompt, role="user"):
+    def create_requests(self, prompt, system_prompt=None, role="user"):
+        messages = []
+        if system_prompt:
+            messages.append({"role": "system", "content": system_prompt})
+        messages.append({"role": role, "content": prompt})
+
         data = {
             "model": self.llm.model,
             "stream": False,
-            "messages": [{"role": role, "content": prompt}],
+            "messages": messages,
         }
         return data
 
@@ -44,13 +49,13 @@ class MAGPIE:
     def generate(self, num_samples, use_cache=False):
         self.display(num_samples)
 
-        request_list = [self.create_requests(self.template, "assistant") for _ in range(num_samples)]
+        request_list = [self.create_requests(self.template,  self.question_prompt, "assistant") for _ in range(num_samples)]
         response_list = self.llm.generate(request_list, use_cache=use_cache)
 
         questions = [response[1]["choices"][0]["message"]["content"] for response in response_list]
         questions = list(filter(self.extract, questions))
 
-        request_list = [self.create_requests(question) for question in questions]
+        request_list = [self.create_requests(question, self.answer_prompt) for question in questions]
         response_list = self.llm.generate(request_list, use_cache=use_cache)
 
         answers = [response[1]["choices"][0]["message"]["content"] for response in response_list]
